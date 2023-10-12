82826147e2c4a6868f9ad2294cdbb7b6568a3e64
clementmwamba <clementmwamba23@gmail.com>
2023-10-12 11:33:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+var customerName ='bob';
+const leastFavoriteCustomer = 'kim';
+
+function upperCaseCustomerName (){
+    return customerName.toUpperCase();
+  
+}
+//console.log(upperCaseCustomerName())
+
+ function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer
+ }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob' 
+    return bestCustomer
+    
+}
+//console.log(overwriteBestCustomer())
+
+function favouriteCustomer(){
+    return leastFavoriteCustomer
+}
+ //console.log(favouriteCustomer())
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'micah'
+    return leastFavoriteCustomer
+}
+ //console.log(changeLeastFavoriteCustomer())
+
+
+
